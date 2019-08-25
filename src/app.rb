9d825d711fb1d7b4bd4e4a70c819bfab397adbfa
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-08-25 19:06:07 -0300

    first commit

---
+++ src/app.rb
@@ -0,0 +1,14 @@
+require 'ruby2d'
+
+set title: "snake game"
+
+set width:800 , heigth: 800
+
+Triangle.new(
+    x1: 320, y1:  50,
+    x2: 540, y2: 430,
+    x3: 100, y3: 430,
+    color: ['red', 'green', 'blue']
+)
+
+show
---
+++ src/model/state.rb
@@ -0,0 +1,21 @@
+# In this is the state of the app
+
+module Model
+  class Coord < Struct.new(row, col)
+  end
+
+  class Food < Coord # herencia
+  end
+
+  class Snake < Struct.new(positions)
+
+  end
+
+  class Grid < Struct.new(rows, cols)
+
+  end
+
+  class State < Struct.new(snake, food, grid)
+
+  end
+end
